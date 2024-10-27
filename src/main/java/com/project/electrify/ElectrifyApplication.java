daa957526c2ee25fed1dc32560300adf00422639
brana555 <brijee.rana@gmail.com>
2024-10-27 15:29:00 +0000

    Intial commit

---
+++ src/main/java/com/project/electrify/ElectrifyApplication.java
@@ -0,0 +1,13 @@
+package com.project.electrify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectrifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectrifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/electrify/ElectrifyApplicationTests.java
@@ -0,0 +1,15 @@
+//package com.project.electrify;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//// write test cases jere
+//
+//@SpringBootTest
+//class ElectrifyApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
